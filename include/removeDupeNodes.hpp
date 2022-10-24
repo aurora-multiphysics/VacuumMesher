05a4e5ceb892edddfafaa8ac1e962b1817e3cd46
Bill Ellis <billellis.07@gmail.com>
2022-10-24 20:21:41 +0100

    Retrieved removeDupeNodes from removeDupeNodes Branch

---
+++ include/removeDupeNodes.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "libmesh/elem.h"
+#include "libmesh/mesh.h"
+#include "libmesh/libmesh.h"
+#include "libmesh/mesh_tools.h"
+#include "libmesh/enum_io_package.h"
+#include "libmesh/enum_elem_type.h"
+#include "libmesh/boundary_info.h"
+#include "surfaceMeshing.hpp"
+#include "utility"
+
+void 
+removeDupeNodes(libMesh::Mesh& mesh);
---
+++ src/removeDupeNodes.cpp
@@ -0,0 +1,48 @@
+#include "removeDupeNodes.hpp"
+
+
+void 
+removeDupeNodes(libMesh::Mesh& mesh)
+{
+    std::cout << "Removing duplicate nodes" << std::endl;
+    std::vector<libMesh::dof_id_type> old_ids;
+    std::vector<std::vector<libMesh::dof_id_type>> nodes_to_elem_map;
+    libMesh::MeshTools::build_nodes_to_elem_map(mesh, nodes_to_elem_map);
+
+    for(auto& node_1 :mesh.node_ptr_range())
+    {
+        libMesh::dof_id_type id_1 = node_1->id();
+        
+        for(auto& node_2: mesh.node_ptr_range())
+        {
+            libMesh::dof_id_type id_2 = node_2->id();
+    
+            if(id_1==id_2)
+            {
+                continue;
+            }
+
+            else if(id_1 > id_2)
+            {
+                continue;
+            }
+
+            else if(((*node_1)(0) == (*node_2)(0)) &&
+                    ((*node_1)(1) == (*node_2)(1)) &&
+                    abs((*node_1)(2) - (*node_2)(2)) < 1e-8)
+            {           
+                libMesh::dof_id_type old_id = id_1 > id_2 ? id_1 : id_2;
+                libMesh::dof_id_type new_id = id_1 < id_2 ? id_1 : id_2;
+
+                std::cout << "Found match, " << old_id << ", " << new_id << std::endl;
+
+                for(auto& elem: nodes_to_elem_map[old_id])
+                {
+                    libMesh::Elem* elem_ptr = mesh.elem_ptr(elem);
+                    elem_ptr->set_node(elem_ptr->local_node(old_id)) = mesh.node_ptr(new_id);
+                } 
+            }
+        }
+    }
+    std::cout << "Dupes removed" << std::endl;
+}
