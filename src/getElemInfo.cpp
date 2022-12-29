38b609552118d5fa1056d086f736d3a11b12c96e
Bill Ellis <billellis.07@gmail.com>
2022-12-29 21:07:48 +0000

    Fixed memory leak in getElemInfo, where a libmesh::Elem ptr was not being deleted

--- src/getElemInfo.cpp
+++ src/getElemInfo.cpp
@@ -5,7 +5,7 @@ getElemInfo(libMesh::ElemType& elem_type, libMesh::ElemType& face_type,
             libMesh::Elem* element, int& num_elem_sides, int& num_face_nodes)
 {
     elem_type = element->type();
-    libMesh::Elem* side = element->side_ptr(0).release();
+    std::unique_ptr<libMesh::Elem> side = element->side_ptr(0);
     num_elem_sides = element->n_sides();
     num_face_nodes = side->n_nodes();            
 }
