5a6e1def54619f7022867aa26653ff316ef67ac2
Bill Ellis <billellis.07@gmail.com>
2023-01-03 15:23:45 +0000

    Removed old python scripts

--- pythonScripts/skin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-search_dir="/home/bill/projects/libmesh-skinning/Meshes/chimBlocks"
-for entry in "${search_dir}"/*.e
-do 
-    ./build/problems/skinner "$(basename "$entry")"
-done
-
-
