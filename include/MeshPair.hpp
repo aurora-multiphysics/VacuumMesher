65f6d2e7a1df801166d1df5c83f48a4af37e703d
Bill Ellis <billellis.07@gmail.com>
2023-02-21 13:18:59 +0000

    Formatted with clang

--- include/MeshPair.hpp
+++ include/MeshPair.hpp
@@ -1,38 +1,39 @@
 #pragma once
 
-#include<stdlib.h>
-#include<string>
-#include<filesystem>
-#include "libmesh/mesh.h"
-#include "libmesh/libmesh.h"
 #include "Utils/libmeshConversions.hpp"
+#include "libmesh/libmesh.h"
+#include "libmesh/mesh.h"
+#include <filesystem>
+#include <stdlib.h>
+#include <string>
 
 // Class to hold both the libmesh and libigl representations of a mesh
-class MeshPair{
+class MeshPair {
 
-    public: 
-        // Default constructor 
-        MeshPair();
+public:
+  // Default constructor
+  MeshPair();
 
-        // Most commonly called constructor; Takes in libMesh::Init object to instantiate meshes
-        MeshPair(const libMesh::LibMeshInit& init);
+  // Most commonly called constructor; Takes in libMesh::Init object to
+  // instantiate meshes
+  MeshPair(const libMesh::LibMeshInit &init);
 
-        // DESTRUCTOR
-        ~MeshPair();
+  // DESTRUCTOR
+  ~MeshPair();
 
-        // Getters for mesh objects
-        libMesh::Mesh& libmeshMesh();
-        Eigen::MatrixXd& iglVerts();
-        Eigen::MatrixXi& iglElems();      
-        
-        // Methods for creating the mesh representation of the other type
-        void createLibmeshAnalogue();
-        void createIglAnalogue();  
+  // Getters for mesh objects
+  libMesh::Mesh &libmeshMesh();
+  Eigen::MatrixXd &iglVerts();
+  Eigen::MatrixXi &iglElems();
 
-    protected:
-    // private:
-        // Mesh objects
-        std::unique_ptr<libMesh::Mesh> libmeshMesh_;
-        Eigen::MatrixXd iglVerts_;
-        Eigen::MatrixXi iglElems_;
+  // Methods for creating the mesh representation of the other type
+  void createLibmeshAnalogue();
+  void createIglAnalogue();
+
+protected:
+  // private:
+  // Mesh objects
+  std::unique_ptr<libMesh::Mesh> libmeshMesh_;
+  Eigen::MatrixXd iglVerts_;
+  Eigen::MatrixXi iglElems_;
 };
