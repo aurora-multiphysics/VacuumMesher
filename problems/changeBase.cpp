ef9695e0dad215ba0ca398e2019167c13cd412cd
Bill Ellis <billellis.07@gmail.com>
2023-02-23 15:03:26 +0000

    updated example problems

--- problems/changeBase.cpp
+++ problems/changeBase.cpp
@@ -1,85 +1,78 @@
+#include "BoundaryGeneration/boundaryGeneration.hpp"
+#include "MeshContainer.hpp"
 #include "Tetrahedralisation/removeDupeNodes.hpp"
-#include "BoundaryGeneration/changeOfBase.hpp"
 #include "Utils/parseFlags.hpp"
-#include "Utils/MeshContainer.hpp"
 #include <chrono>
 
-int main(int argc, const char** argv)
-{
-    inputFlags flags{parse_settings(argc, argv)};
-    flags.setSwitches();
-    // Setup simple argv for use with libMesh init
-    std::string appName(argv[0]);
-    std::vector<char*> libmeshArgv = {(char*)appName.data()};
-    std::cout << flags.tetSettings << std::endl;
-    // Set up appropriate file names
-    
-    libMesh::LibMeshInit init(libmeshArgv.size() - 1, libmeshArgv.data());
-    //Create mesh object to store original model mesh
-    MeshContainer meshes(init, flags.infile.value());
+int main(int argc, const char **argv) {
+  inputFlags flags(parse_settings(argc, argv));
+  flags.setSwitches();
+  // Setup simple argv for use with libMesh init
+  std::string appName(argv[0]);
+  std::vector<char *> libmeshArgv = {(char *)appName.data()};
+  std::cout << flags.tetSettings << std::endl;
+  // Set up appropriate file names
 
-    // If user has not specified the length of the boundary, then it is set here
-    if(!flags.bound_len.has_value())
-    {
-        // create bounding box around mesh
-        auto box = libMesh::MeshTools::create_bounding_box(meshes.userMesh().libmeshMesh());
-        
-        // Use bounding points to determine a suitable boundary length
-        flags.bound_len = 1.5*(box.max() - box.min()).norm();
-    }
-  
-    // Multimap to store which sides of the elements are boundary sides (i.e. which sides have the null neighbor)
-    getSurface(meshes.userMesh().libmeshMesh(),
-               meshes.skinnedMesh().libmeshMesh(),
-               meshes.surfaceFaceMap(),
-               flags.verbose);
+  libMesh::LibMeshInit init(libmeshArgv.size() - 1, libmeshArgv.data());
+  // Create mesh object to store original model mesh
+  MeshContainer meshes(init, flags.infile.value());
 
-    // Convert surface mesh to libIGL compatible data structures
-    meshes.skinnedMesh().createIglAnalogue();
+  // If user has not specified the length of the boundary, then it is set here
+  if (!flags.boundLen.has_value()) {
+    // create bounding box around mesh
+    auto box = libMesh::MeshTools::create_bounding_box(
+        meshes.userMesh().libmeshMesh());
 
-    // Get seed points
-    Eigen::MatrixXd seed_points = getSeeds(meshes.skinnedMesh().libmeshMesh(), 1e-04);
-    
-    // Turn surfMesh into boundaryMesh
-    getBasisChangeMesh(meshes.userMesh().libmeshMesh(),
+    // Use bounding points to determine a suitable boundary length
+    flags.boundLen = 1.5 * (box.max() - box.min()).norm();
+  }
+
+  // Multimap to store which sides of the elements are boundary sides (i.e.
+  // which sides have the null neighbor)
+  getSurface(meshes.userMesh().libmeshMesh(),
+             meshes.skinnedMesh().libmeshMesh(), meshes.surfaceFaceMap(),
+             flags.verbose);
+
+  // Convert surface mesh to libIGL compatible data structures
+  meshes.skinnedMesh().createIglAnalogue();
+
+  // Get seed points
+  Eigen::MatrixXd seed_points =
+      getSeeds(meshes.skinnedMesh().libmeshMesh(), 1e-04);
+
+  // Turn surfMesh into boundaryMesh
+  generateCoilBoundary(meshes.userMesh().libmeshMesh(),
                        meshes.boundaryMesh().iglVerts(),
-                       meshes.boundaryMesh().iglElems(),
-                       flags.bound_len.value(),
-                       flags.bound_subd.value(),
-                       flags.triSettings);
+                       meshes.boundaryMesh().iglElems(), flags.boundLen.value(),
+                       flags.boundSubd.value(), flags.triSettings);
 
-    // If verbose flag is set, output the boundaryMesh
-    if(flags.verbose)
-    {
-        meshes.boundaryMesh().createLibmeshAnalogue();
-        meshes.boundaryMesh().libmeshMesh().write(meshes.boundFilename_);
-    }
-    
-    // Tolerance for rTree combining of meshes
-    const double tol = 1e-05;
+  // If verbose flag is set, output the boundaryMesh
+  if (flags.verbose) {
+    meshes.boundaryMesh().createLibmeshAnalogue();
+    meshes.boundaryMesh().libmeshMesh().write(meshes.boundFilename_);
+  }
 
-    // Combine the boundary with the surface mesh to create a closed manifold we can use for tetrahedrelisation
-    combineMeshes(tol, 
-                meshes.boundaryMesh().iglVerts(),
-                meshes.boundaryMesh().iglElems(),
-                meshes.skinnedMesh().iglVerts(),
-                meshes.skinnedMesh().iglElems());
-    
-    // Tetrahedralise everything
-    tetrahedraliseVacuumRegion(meshes.boundaryMesh().iglVerts(),
-                               meshes.boundaryMesh().iglElems(),
-                               meshes.vacuumMesh().libmeshMesh(),
-                               seed_points,
-                               flags.tetSettings);
+  // Tolerance for rTree combining of meshes
+  const double tol = 1e-05;
 
-    // 
-    combineMeshes(tol,
-                  meshes.userMesh().libmeshMesh(),
-                  meshes.vacuumMesh().libmeshMesh(),
-                  meshes.surfaceFaceMap());
-    
-    meshes.userMesh().libmeshMesh().write("neatnessTest.e");
-    // // Write the mesh to either the value provided in the input flags, or a default filepath
+  // Combine the boundary with the surface mesh to create a closed manifold we
+  // can use for tetrahedrelisation
+  combineMeshes(
+      tol, meshes.boundaryMesh().iglVerts(), meshes.boundaryMesh().iglElems(),
+      meshes.skinnedMesh().iglVerts(), meshes.skinnedMesh().iglElems());
 
-    return 0;
+  // Tetrahedralise everything
+  tetrahedraliseVacuumRegion(
+      meshes.boundaryMesh().iglVerts(), meshes.boundaryMesh().iglElems(),
+      meshes.vacuumMesh().libmeshMesh(), seed_points, flags.tetSettings);
+
+  //
+  combineMeshes(tol, meshes.userMesh().libmeshMesh(),
+                meshes.vacuumMesh().libmeshMesh(), meshes.surfaceFaceMap());
+
+  meshes.userMesh().libmeshMesh().write(meshes.vacuumFilename_);
+  // // Write the mesh to either the value provided in the input flags, or a
+  // default filepath
+
+  return 0;
 }
--- problems/skinMesh.cpp
+++ problems/skinMesh.cpp
@@ -1,66 +1,46 @@
-#include<iostream>
-#include "libmesh/libmesh.h"
-#include "libmesh/enum_io_package.h"
-#include "libmesh/enum_elem_type.h"
-#include "Tetrahedralisation/tetMaker.hpp"
-#include "SurfaceMeshing/surfaceMeshing.hpp"
 #include "MeshContainer.hpp"
-#include <chrono>
+#include "SurfaceMeshing/surfaceMeshing.hpp"
+#include "Tetrahedralisation/tetMaker.hpp"
 #include "algorithm"
-
+#include "libmesh/enum_elem_type.h"
+#include "libmesh/enum_io_package.h"
+#include "libmesh/libmesh.h"
+#include <chrono>
+#include <iostream>
 
 using namespace libMesh;
 
-int main(int argc, char** argv)
-{
-    try
-    {   
-        if(argc == 1)
-        {
-            throw 0;
-        }
-    } catch(...)
-    {
-        std::cout << "Error, mesh file argument is required." << std::endl;
-        return -1;
+int main(int argc, char **argv) {
+  try {
+    if (argc == 1) {
+      throw 0;
     }
-    std::string appName(argv[0]);
-    std::vector<char*> libmeshArgv = {(char*)appName.data()};
-    std::string filename(argv[1]);
-    
-    //Initialise libmesh functions and mpi    
-    LibMeshInit init(libmeshArgv.size() - 1, libmeshArgv.data());
-    // Mesh container object, that has ownership of the mesh, surfaceMesh, Vacuum Mesh
-    MeshContainer meshes(init, filename);
+  } catch (...) {
+    std::cout << "Error, mesh file argument is required." << std::endl;
+    return -1;
+  }
+  std::string appName(argv[0]);
+  std::vector<char *> libmeshArgv = {(char *)appName.data()};
+  std::string filename(argv[1]);
 
-    //Read volume mesh
-    auto start1 = std::chrono::steady_clock::now();
-    getSurface(meshes.userMesh().libmeshMesh(), 
-               meshes.skinnedMesh().libmeshMesh(), 
-               meshes.surfaceFaceMap(), true, 
-               meshes.skinFilename_);
+  // Initialise libmesh functions and mpi
+  LibMeshInit init(libmeshArgv.size() - 1, libmeshArgv.data());
+  // Mesh container object, that has ownership of the mesh, surfaceMesh, Vacuum
+  // Mesh
+  MeshContainer meshes(init, filename);
 
-    auto end1 = std::chrono::steady_clock::now();
-    std::cout << "Elapsed time in milliseconds: "
-    << std::chrono::duration_cast<std::chrono::milliseconds>(end1 - start1).count()
-    << " ms" << std::endl;
-    
-    
-    
+  // Read volume mesh
+  auto start1 = std::chrono::steady_clock::now();
+  getSurface(meshes.userMesh().libmeshMesh(),
+             meshes.skinnedMesh().libmeshMesh(), meshes.surfaceFaceMap(), true,
+             meshes.skinFilename_);
 
+  auto end1 = std::chrono::steady_clock::now();
+  std::cout << "Elapsed time in milliseconds: "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(end1 -
+                                                                     start1)
+                   .count()
+            << " ms" << std::endl;
 
-
-    // std::vector<std::vector<libMesh::dof_id_type>> groups;
-
-    // auto start = std::chrono::steady_clock::now();
-    // // groupElems(surfaceMesh, groups);
-    // // saveGroupedElems(init, surfaceMesh, groups, "block6_");
-    // auto end = std::chrono::steady_clock::now();
-    // std::cout << "Elapsed time in milliseconds: "
-    // << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
-    // << " ms" << std::endl;
-    // surfaceMesh.write("surgery.e");
-
-    return 0;
+  return 0;
 }
-
