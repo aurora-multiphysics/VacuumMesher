44f8cd9a2ddc070b35ddd8de07d6dbccf28c8910
Bill Ellis <billellis.07@gmail.com>
2023-03-14 17:33:41 +0000

    Updated skinning example to use CL inputs

--- bin/skinMesh.cpp
+++ bin/skinMesh.cpp
@@ -5,7 +5,7 @@
 #include "Utils/parseFlags.hpp"
 #include <chrono>
 
-using namespace libMesh;
+// using namespace libMesh;
 
 int main(int argc, const char **argv) {
   inputFlags flags(parse_settings(argc, argv));
@@ -14,14 +14,13 @@ int main(int argc, const char **argv) {
   // Setup simple argv for use with libMesh init
   std::string appName(argv[0]);
   std::vector<char *> libmeshArgv = {(char *)appName.data()};
-  std::cout << flags.tetSettings << std::endl;
 
   // Initialise libmesh functions and mpi
-  LibMeshInit init(libmeshArgv.size() - 1, libmeshArgv.data());
+  libMesh::LibMeshInit init(libmeshArgv.size() - 1, libmeshArgv.data());
   // Mesh container object, that has ownership of the mesh, surfaceMesh, Vacuum
   MeshContainer meshes(init, flags.infile.value());
 
   // Read volume mesh
-  getSurface(meshes.userMesh().libmeshMesh(), meshes.skinnedMesh().libmeshMesh(), &(meshes.surfaceFaceMap()));
+  getSurface(meshes.userMesh().libmeshMesh(), meshes.skinnedMesh().libmeshMesh(), &(meshes.surfaceFaceMap()), true);
   return 0;
 }
