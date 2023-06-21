ecf9b51fe40cbde9a830e3c9e56066248ee37a1d
Bill Ellis <billellis.07@gmail.com>
2023-06-21 18:34:56 +0100

    Removed some old example bins and updated CMakeLists

--- bin/vcoil.cpp
+++ bin/vcoil.cpp
@@ -5,59 +5,69 @@
 #include <chrono>
 
 int main(int argc, const char **argv) {
+
+  // Parse input flags 
   inputFlags flags(parse_settings(argc, argv));
+
+  // use input flags to set tetgen/triangle switches
   flags.setSwitches();
+
   // Setup simple argv for use with libMesh init
   std::string appName(argv[0]);
   std::vector<char *> libmeshArgv = {(char *)appName.data()};
   std::cout << flags.tetSettings << std::endl;
 
-  // 
+  //Initialise libmesh 
   libMesh::LibMeshInit init(libmeshArgv.size() - 1, libmeshArgv.data());
-  // Create mesh object to store original model mesh
+
+  // Create MeshContainer object to help organise all of the meshes 
   MeshContainer meshes(init, flags.infile.value());
 
   // If user has not specified the length of the boundary, then it is set here
   if (!flags.boundLen.has_value()) {
-    // create bounding box around mesh
+
+// create bounding box around mesh
     auto box = libMesh::MeshTools::create_bounding_box(
         meshes.userMesh().libmeshMesh());
 
-    // Use bounding points to determine a suitable boundary length
+// Use bounding points to determine a suitable boundary length
     flags.boundLen = 1.5 * (box.max() - box.min()).norm();
   }
 
-  // Multimap to store which sides of the elements are boundary sides (i.e.
-  // which sides have the null neighbor)
+  std::cout << "Getting surface" << std::endl;
+
+  // Generate the skinned mesh, and store it in meshes.skinnedMesh. The surfaceFaceMap
+  // is a multimap that maps from element ID's, to boundary side id's (i.e.
+  // the faces of that element that have a null neighbor). This information is later used to create
+  // a sideset that represents the boundary of the original mesh.
   getSurface(meshes.userMesh().libmeshMesh(),
-             meshes.skinnedMesh().libmeshMesh(), &meshes.surfaceFaceMap(),
-             true);
+             meshes.skinnedMesh().libmeshMesh(), &meshes.surfaceFaceMap());
 
   // Convert surface mesh to libIGL compatible data structures
   meshes.skinnedMesh().createIglAnalogue();
 
-  // Get seed points
+  // Get seed points. Seeding points are used to 
   Eigen::MatrixXd seed_points =
       getSeeds(meshes.skinnedMesh().libmeshMesh(), 1e-04);
 
   // Turn surfMesh into boundaryMesh
-  generateCoilBoundary(meshes.userMesh().libmeshMesh(),
-                       meshes.boundaryMesh().libmeshMesh(),
-                       flags.boundLen.value(),
-                       flags.boundSubd.value(), flags.triSettings);
 
-  // We got the boundary mesh as a libmesh mesh, but we need its IGL version as well
-  meshes.boundaryMesh().createIglAnalogue();
+
+    generateCoilBoundary(meshes.userMesh().libmeshMesh(), meshes.boundaryMesh().libmeshMesh(),
+                         flags.boundLen.value(), flags.boundSubd.value(), flags.triSettings);
+
+
+
+
   // If verbose flag is set, output the boundaryMesh
   if (flags.verbose) {
     meshes.boundaryMesh().createLibmeshAnalogue();
     meshes.boundaryMesh().libmeshMesh().write(meshes.boundFilename_);
-  }
+  }                         
 
-  // Tolerance for rTree combining of meshes
-  const double tol = 1e-05;
 
-  // Combine the boundary with the surface mesh to create a closed manifold we
+    const double tol = 1e-05;
+      // Combine the boundary with the surface mesh to create a closed manifold we
   // can use for tetrahedrelisation
   combineMeshes(
       tol, meshes.boundaryMesh().iglVerts(), meshes.boundaryMesh().iglElems(),
@@ -73,8 +83,10 @@ int main(int argc, const char **argv) {
                 meshes.vacuumMesh().libmeshMesh(), meshes.surfaceFaceMap());
 
   meshes.userMesh().libmeshMesh().write(meshes.vacuumFilename_);
-  // // Write the mesh to either the value provided in the input flags, or a
-  // default filepath
+
+
+  // We got the boundary mesh as a libmesh mesh, but we need its IGL version as well
+  meshes.boundaryMesh().createIglAnalogue();
 
   return 0;
 }
--- bin/vmesh.cpp
+++ bin/vmesh.cpp
@@ -5,75 +5,75 @@
 #include <chrono>
 
 int main(int argc, const char **argv) {
+
+  // Parse input flags 
   inputFlags flags(parse_settings(argc, argv));
+
+  // use input flags to set tetgen/triangle switches
   flags.setSwitches();
+
   // Setup simple argv for use with libMesh init
   std::string appName(argv[0]);
   std::vector<char *> libmeshArgv = {(char *)appName.data()};
   std::cout << flags.tetSettings << std::endl;
 
-  // 
+  //Initialise libmesh 
   libMesh::LibMeshInit init(libmeshArgv.size() - 1, libmeshArgv.data());
-  // Create mesh object to store original model mesh
+
+  // Create MeshContainer object to help organise all of the meshes 
   MeshContainer meshes(init, flags.infile.value());
 
   // If user has not specified the length of the boundary, then it is set here
   if (!flags.boundLen.has_value()) {
-    // create bounding box around mesh
+
+// create bounding box around mesh
     auto box = libMesh::MeshTools::create_bounding_box(
         meshes.userMesh().libmeshMesh());
 
-    // Use bounding points to determine a suitable boundary length
+// Use bounding points to determine a suitable boundary length
     flags.boundLen = 1.5 * (box.max() - box.min()).norm();
   }
-  
-  // Multimap to store which sides of the elements are boundary sides (i.e.
-  // which sides have the null neighbor)
+
+  std::cout << "Getting surface" << std::endl;
+
+  // Generate the skinned mesh, and store it in meshes.skinnedMesh. The surfaceFaceMap
+  // is a multimap that maps from element ID's, to boundary side id's (i.e.
+  // the faces of that element that have a null neighbor). This information is later used to create
+  // a sideset that represents the boundary of the original mesh.
   getSurface(meshes.userMesh().libmeshMesh(),
              meshes.skinnedMesh().libmeshMesh(), &meshes.surfaceFaceMap());
+
   // Convert surface mesh to libIGL compatible data structures
   meshes.skinnedMesh().createIglAnalogue();
 
-  // Get seed points
+  // Get seed points. Seeding points are used to 
   Eigen::MatrixXd seed_points =
       getSeeds(meshes.skinnedMesh().libmeshMesh(), 1e-04);
 
   // Turn surfMesh into boundaryMesh
-  // generateCoilBoundary(meshes.userMesh().libmeshMesh(),
-  //                      meshes.boundaryMesh().libmeshMesh(),
-  //                      flags.boundLen.value(),
-  //                      flags.boundSubd.value(), flags.triSettings);
-
   addBoundary(meshes.skinnedMesh().libmeshMesh(), meshes.boundaryMesh().libmeshMesh(),
               flags.boundLen.value(),
               flags.boundSubd.value(), flags.triSettings);
 
-  std::cout << "Getting surface" << std::endl;
-
 
   // We got the boundary mesh as a libmesh mesh, but we need its IGL version as well
   meshes.boundaryMesh().createIglAnalogue();
+
   // If verbose flag is set, output the boundaryMesh
   if (flags.verbose) {
     meshes.boundaryMesh().createLibmeshAnalogue();
     meshes.boundaryMesh().libmeshMesh().write(meshes.boundFilename_);
   }
 
-  // Tolerance for rTree combining of meshes
+  // rTree tolerance for combining meshes
   const double tol = 1e-05;
-
-  // Combine the boundary with the surface mesh to create a closed manifold we
-  // can use for tetrahedrelisation
-  // combineMeshes(
-  //     tol, meshes.boundaryMesh().iglVerts(), meshes.boundaryMesh().iglElems(),
-  //     meshes.skinnedMesh().iglVerts(), meshes.skinnedMesh().iglElems());
 
   // Tetrahedralise everything
   tetrahedraliseVacuumRegion(
       meshes.boundaryMesh().iglVerts(), meshes.boundaryMesh().iglElems(),
       meshes.vacuumMesh().libmeshMesh(), seed_points, flags.tetSettings);
 
-  //
+  // Combine the meshes 
   combineMeshes(tol, meshes.userMesh().libmeshMesh(),
                 meshes.vacuumMesh().libmeshMesh(), meshes.surfaceFaceMap());
 
