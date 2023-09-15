4ed12b82787659a0bf104d89547073aebee28dc2
billellis <billellis.07@gmail.com>
2023-09-15 16:18:45 +0100

    Updated styling in removeDupeNodes to be consistent with rest of code base

--- VacuumMeshing/src/Utils/removeDupeNodes.cpp
+++ VacuumMeshing/src/Utils/removeDupeNodes.cpp
@@ -19,9 +19,9 @@ void createTree(nodeTree &rtree, Eigen::MatrixXd &V, const double &tol) {
   }
 }
 
-void createTree(nodeTree &rtree, libMesh::Mesh &meshOne, const double &tol) {
+void createTree(nodeTree &rtree, libMesh::Mesh &mesh_one, const double &tol) {
   // Add all existing verts to rTree
-  for (auto &node : meshOne.local_node_ptr_range()) {
+  for (auto &node : mesh_one.local_node_ptr_range()) {
     std::array node_coords = {(*node)(0), (*node)(1), (*node)(2)};
     Box node_box = Box(node_coords, tol, node->id());
     insertNode(rtree, node_box);
@@ -60,15 +60,15 @@ bool searchTree(nodeTree &rtree, const double &tol,
   return true;
 }
 
-void combineMeshes(const double &tol, libMesh::Mesh &meshOne,
-                   libMesh::Mesh &meshTwo,
-                   std::multimap<unsigned int, unsigned int> surfaceFaceMap) {
+void combineMeshes(const double &tol, libMesh::Mesh &mesh_one,
+                   libMesh::Mesh &mesh_two,
+                   std::multimap<unsigned int, unsigned int> surface_face_map) {
   std::set<libMesh::subdomain_id_type> sub_ids;
-  meshOne.subdomain_ids(sub_ids);
-  libMesh::subdomain_id_type vacId = 2;
+  mesh_one.subdomain_ids(sub_ids);
+  libMesh::subdomain_id_type vac_id = 2;
   if (!sub_ids.empty()) {
-    vacId = 1 + (*(sub_ids.rbegin()));
-    std::cout << "vac Id will be " << vacId << std::endl;
+    vac_id = 1 + (*(sub_ids.rbegin()));
+    std::cout << "vac Id will be " << vac_id << std::endl;
   }
 
   // Instantiate an rTree. Using a rTree data structure significantly reduces
@@ -79,19 +79,19 @@ void combineMeshes(const double &tol, libMesh::Mesh &meshOne,
   // will
   //  check the nodes of the part mesh against this tree to search for duplicate
   //  nodes
-  createTree(rtree, meshOne, tol);
+  createTree(rtree, mesh_one, tol);
 
   // This is a map which helps us keep track of the node id's of the duplicate
   // nodes. For example, if Node 4 in the surface mesh and Node 6 in the vacuum
   // mesh are the same, "id_map[4]" will return 6
   std::map<unsigned int, unsigned int> id_map;
-  for (auto &node : meshTwo.local_node_ptr_range()) {
+  for (auto &node : mesh_two.local_node_ptr_range()) {
     // Create box representing the node to search rTree with
     std::array node_coords = {(*node)(0), (*node)(1), (*node)(2)};
     Box node_box = Box(node_coords, tol, node->id());
     // Get number of nodes currently in the mesh, so if we have to add a node
     //  we know what id to give it (node_indent + 1)
-    int node_offset = meshOne.n_nodes();
+    int node_offset = mesh_one.n_nodes();
     // If there are no matches in the vacuumMesh, then we add the point
     if (!searchTree(rtree, tol, id_map, node_offset, node_box)) {
       // If there are no matches in the vacuumMesh, then we add the point
@@ -106,24 +106,25 @@ void combineMeshes(const double &tol, libMesh::Mesh &meshOne,
     pnt[1] = (*node)(1);
     pnt[2] = (*node)(2);
     libMesh::Point xyz(pnt[0], pnt[1], pnt[2]);
-    meshOne.add_point(xyz, id_map[node->id()]);
+    mesh_one.add_point(xyz, id_map[node->id()]);
   }
 
   // Loop adds all elements from original geometry to vacuum mesh. The id_map is
   // used to make sure the correct connectivity is used where there are
   // duplicate nodes
-  for (auto &elem : libMesh::as_range(meshTwo.local_elements_begin(),
-                                      meshTwo.local_elements_end())) {
-    unsigned int el_id = 1 + meshOne.n_elem();
+  for (auto &elem : libMesh::as_range(mesh_two.local_elements_begin(),
+                                      mesh_two.local_elements_end())) {
+    unsigned int el_id = 1 + mesh_one.n_elem();
     // Element type being build must be consistent between meshes
     libMesh::Elem *new_elem =
-        libMesh::Elem::build(meshOne.elem_ptr(0)->type()).release();
+        libMesh::Elem::build(mesh_one.elem_ptr(0)->type()).release();
     for (int j = 0; j < new_elem->n_nodes(); j++) {
-      new_elem->set_node(j) = meshOne.node_ptr(id_map[elem->node_ptr(j)->id()]);
+      new_elem->set_node(j) =
+          mesh_one.node_ptr(id_map[elem->node_ptr(j)->id()]);
     }
     new_elem->set_id(el_id);
-    new_elem->subdomain_id() = vacId;
-    meshOne.add_elem(new_elem);
+    new_elem->subdomain_id() = vac_id;
+    mesh_one.add_elem(new_elem);
 
     // Makes the boundary between the original geometry and the vacuum mesh a
     // sideset in the mesh, and names it "vacuum_boundary"
@@ -131,23 +132,23 @@ void combineMeshes(const double &tol, libMesh::Mesh &meshOne,
     // Set boundary name!
   }
 
-  for (auto boundSide = surfaceFaceMap.begin();
-       boundSide != surfaceFaceMap.end(); boundSide++) {
+  for (auto boundSide = surface_face_map.begin();
+       boundSide != surface_face_map.end(); boundSide++) {
     // std::cout << boundSide->first << std::endl;
-    meshOne.get_boundary_info().add_side(boundSide->first, boundSide->second,
-                                         5);
+    mesh_one.get_boundary_info().add_side(boundSide->first, boundSide->second,
+                                          5);
   }
-  meshOne.boundary_info->set_sideset_name_map()[5] = "part_boundary";
-  meshOne.subdomain_name(vacId) = "vacuum_region";
+  mesh_one.boundary_info->set_sideset_name_map()[5] = "part_boundary";
+  mesh_one.subdomain_name(vac_id) = "vacuum_region";
   // Prepare the mesh for use. This libmesh method does some id renumbering etc,
   // generally a good idea to call it after constructing a mesh
-  meshOne.prepare_for_use();
+  mesh_one.prepare_for_use();
 }
 
-// Version of the method that doesn't require a surfaceFaceMap, just combines
+// Version of the method that doesn't require a surface_face_map, just combines
 // the meshes
-void combineMeshes(const double &tol, libMesh::Mesh &meshOne,
-                   libMesh::Mesh &meshTwo) {
+void combineMeshes(const double &tol, libMesh::Mesh &mesh_one,
+                   libMesh::Mesh &mesh_two) {
   // Instantiate an rTree. Using a rTree data structure significantly reduces
   // the amount of time taken to discover duplicate nodes.
   nodeTree rtree;
@@ -156,7 +157,7 @@ void combineMeshes(const double &tol, libMesh::Mesh &meshOne,
   // will
   //  check the nodes of the part mesh against this tree to search for duplicate
   //  nodes
-  createTree(rtree, meshOne, tol);
+  createTree(rtree, mesh_one, tol);
 
   // This is a map which helps us keep track of the node id's of the duplicate
   // nodes. For example, if Node 4 in the surface mesh and Node 6 in the vacuum
@@ -165,12 +166,12 @@ void combineMeshes(const double &tol, libMesh::Mesh &meshOne,
 
   // Loop over all the nodes in the original geometry. If the node is not a
   // duplicate, add it to the mesh , if the node is a duplicate, it is
-  for (auto &node : meshTwo.local_node_ptr_range()) {
+  for (auto &node : mesh_two.local_node_ptr_range()) {
     // Create box representing the node to search rTree with
     std::array node_coords = {(*node)(0), (*node)(1), (*node)(2)};
     Box node_box = Box(node_coords, tol, node->id());
     // Update node count and henceforth the offset for numbering new nodes
-    int node_offset = meshOne.n_nodes();
+    int node_offset = mesh_one.n_nodes();
 
     if (!searchTree(rtree, tol, id_map, node_offset, node_box)) {
       // If there are no matches in the vacuumMesh, then we add the point
@@ -185,28 +186,29 @@ void combineMeshes(const double &tol, libMesh::Mesh &meshOne,
     pnt[1] = (*node)(1);
     pnt[2] = (*node)(2);
     libMesh::Point xyz(pnt[0], pnt[1], pnt[2]);
-    meshOne.add_point(xyz, id_map[node->id()]);
+    mesh_one.add_point(xyz, id_map[node->id()]);
   }
 
   // Loop adds all elements from original geometry to vacuum mesh. The id_map is
   // used to make sure the correct connectivity is used where there are
   // duplicate nodes
-  for (auto &elem : libMesh::as_range(meshTwo.local_elements_begin(),
-                                      meshTwo.local_elements_end())) {
-    unsigned int el_id = 1 + meshOne.n_elem();
+  for (auto &elem : libMesh::as_range(mesh_two.local_elements_begin(),
+                                      mesh_two.local_elements_end())) {
+    unsigned int el_id = 1 + mesh_one.n_elem();
     // Element type being build must be consistent between meshes
     libMesh::Elem *new_elem =
-        libMesh::Elem::build(meshOne.elem_ptr(0)->type()).release();
+        libMesh::Elem::build(mesh_one.elem_ptr(0)->type()).release();
     for (int j = 0; j < new_elem->n_nodes(); j++) {
-      new_elem->set_node(j) = meshOne.node_ptr(id_map[elem->node_ptr(j)->id()]);
+      new_elem->set_node(j) =
+          mesh_one.node_ptr(id_map[elem->node_ptr(j)->id()]);
     }
     new_elem->set_id(el_id);
     // new_elem->subdomain_id() = 2;
-    meshOne.add_elem(new_elem);
+    mesh_one.add_elem(new_elem);
   }
   // Prepare the mesh for use. This libmesh method does some id renumbering etc,
   // generally a good idea to call it after constructing a mesh
-  meshOne.prepare_for_use();
+  mesh_one.prepare_for_use();
 }
 
 void combineMeshes(const double &tol, Eigen::MatrixXd &V1, Eigen::MatrixXi &F1,
